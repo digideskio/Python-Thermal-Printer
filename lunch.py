ab2f9997ae8a2e5ca57d6d34535f1c07cb315efd
Wade Wegner <wade.wegner@gmail.com>
2014-01-11 17:54:55 -0700

    fixing

--- lunch.py
+++ lunch.py
@@ -3,30 +3,21 @@ import json
 import re
 import urllib2
 import time
+from __future__ import print_function
 from Adafruit_Thermal import *
+from xml.dom.minidom import parseString
 
+printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
 today = time.strftime("%Y-%m-%d")
+
 text = urllib2.urlopen('http://dcsd.nutrislice.com/menu/meadow-view/lunch/').read()
 menus = json.loads(re.search(r"bootstrapData\['menuMonthWeeks'\]\s*=\s*(.*);", text).group(1))
 
-try:
+days = itertools.chain.from_iterable(menu['days'] for menu in menus)
 
-	days = itertools.chain.from_iterable(menu['days'] for menu in menus)
-	day = next(itertools.dropwhile(lambda day: day['date'] != '2014-01-13', days), 1)
+day = next(itertools.dropwhile(lambda day: day['date'] != '2014-01-13', days), None)
 
-	printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
-
-	printer.inverseOn()
-	printer.printlf('{:^32}'.format("Lunch for " + today))
-	printer.inverseOff()
-
-	results = '\n'.join(item['food']['name'] for item in day['menu_items'])
-
-	printer.printlf(results)
-
-	print results
-
-
-except:
-
-	print "No lunch today at school."
+if day:
+    print '\n'.join(item['food']['name'] for item in day['menu_items'])
+else:
+    print 'Day not found.'
