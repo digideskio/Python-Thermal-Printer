87173af9eedb84116ae5f5d69e8b3cd89fed4e04
Wade Wegner <wade.wegner@gmail.com>
2014-01-11 18:32:40 -0700

    Ready?

--- lunch.py
+++ lunch.py
@@ -7,27 +7,19 @@ import re
 import urllib2
 import time
 
-
-
 printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
-
-printer.print("testing 123")
-
 today = time.strftime("%Y-%m-%d")
-
-#printer.printlf(today)
 
 text = urllib2.urlopen('http://dcsd.nutrislice.com/menu/meadow-view/lunch/').read()
 menus = json.loads(re.search(r"bootstrapData\['menuMonthWeeks'\]\s*=\s*(.*);", text).group(1))
-
 days = itertools.chain.from_iterable(menu['days'] for menu in menus)
-
 day = next(itertools.dropwhile(lambda day: day['date'] != '2014-01-13', days), None)
 
 if day:
 
-	menu_items = '\n'.join(item['food']['name'] for item in day['menu_items'])
-	printer.print(menu_items)
+    menu_items = '\n'.join(item['food']['name'] for item in day['menu_items'])
+    printer.print(menu_items)
     print(menu_items)
+
 else:
     print('Day not found.')
