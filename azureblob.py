d4ed9773490c995102c1ee4d4094f8b935c1a110
Wade Wegner <wade.wegner@gmail.com>
2015-11-15 20:35:32 -0800

    updated

--- azureblob.py
+++ azureblob.py
@@ -1,4 +1,9 @@
 from azure.storage.blob import BlobService
+import sys
+
+name = str(sys.argv[0])
+print(name)
+
 
 blob_service = BlobService(account_name='rpprinter', account_key='JMak74DHgi654oBKBGEWKPaVPynUA+w0H8Po9y5gqWwt8HVyQ9NLLg8aTWzRjC3eD/t2tVsswzXVs9NUJ9WjPg==')
 
