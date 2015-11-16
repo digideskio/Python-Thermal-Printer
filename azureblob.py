d0c22f3e9438bb373555292bcc7ed57210c89ca5
Wade Wegner <wade.wegner@gmail.com>
2015-11-15 16:31:14 -0800

    Added azure blobs

---
+++ azureblob.py
@@ -0,0 +1,13 @@
+from azure.storage.blob import BlobService
+
+blob_service = BlobService(account_name='rpprinter', account_key='JMak74DHgi654oBKBGEWKPaVPynUA+w0H8Po9y5gqWwt8HVyQ9NLLg8aTWzRjC3eD/t2tVsswzXVs9NUJ9WjPg==')
+
+blob_service.create_container('printerpics', x_ms_blob_public_access='container')
+
+blob_service.put_block_blob_from_path(
+    'printerpics',
+    'image.jpg',
+    'image.jpg',
+    x_ms_blob_content_type='image/jpg'
+)
+
